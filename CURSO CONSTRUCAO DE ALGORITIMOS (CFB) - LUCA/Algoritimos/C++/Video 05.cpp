e403b6ab3e1b38e89f417687fe86c08a31e3ebb8
Luca Nogueira de Souza <lucanogueira.desouza@gmail.com>
2025-06-29 00:35:07 -0300

    29/06
    adicionado informações do vídeo 5

---
+++ CURSO CONSTRUCAO DE ALGORITIMOS (CFB) - LUCA/Algoritimos/C++/Video 05.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Tipos primitivos:" << endl;
+    float peso = 58.9;
+    double quebrado_gigante = 10.222732312374183123948;
+    int altura = 189;
+    long int numero_imenso = 19213814714612312831283194;
+    char letra = "L";
+    string nome = "Luca";
+    bool estado_gato = true
+
+    return 0;
+}
