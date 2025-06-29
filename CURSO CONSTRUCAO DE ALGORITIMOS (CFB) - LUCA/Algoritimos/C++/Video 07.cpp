e7586160394b4c1f59e4a9e76e566c9818e66149
Luca Nogueira de Souza <lucanogueira.desouza@gmail.com>
2025-06-29 19:52:04 -0300

    29/06
    Adicionado informações do vídeo 07

---
+++ CURSO CONSTRUCAO DE ALGORITIMOS (CFB) - LUCA/Algoritimos/C++/Video 07.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num; // criando varivel
+
+    cout << "Digite um numero inteiro: ";
+    cin >> num; // armazenando o valor digitado pelo teclado na variavel num
+
+    cout << "Numero digitado: " << num << endl; // exibindo o valor digitado pelo teclado na tela do usuario
+
+   return 0;
+}
---
+++ CURSO CONSTRUCAO DE ALGORITIMOS (CFB) - LUCA/Algoritimos/C/Video07.c
@@ -0,0 +1,24 @@
+#include "stdio.h"
+#include <locale.h>
+
+int main() {
+   setlocale(LC_ALL, "Portuguese"); // Habilitar a acentuação
+
+   printf("\===============\n");
+   int ano, aula, edicao;
+   printf("Digite o ano atual: ");
+   scanf("%i", &ano);
+
+   printf("Digite a edição que está assistindo: ");
+   scanf("%i", &edicao);
+
+   printf("Digite a aula que está assistindo: ");
+   scanf("%i", &aula);
+   printf("\n================\n");
+
+
+   printf("CFB Cursos - %i° Edição\n\nAno: %i\n\nAula atual: %i", edicao, ano, aula);
+   printf("\n================\n");
+
+   return 0;
+}
